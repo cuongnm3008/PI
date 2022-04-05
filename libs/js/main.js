a42224cf66617babc450190dcd6a0c7a57640f30
DESKTOP-3Q6SPE5\Orenda <cuongnguyenmanh3008@gmail.com>
2022-04-05 17:05:06 +0700

    a

---
+++ libs/js/main.js
@@ -0,0 +1,72 @@
+
+
+// Giải pháp quản lý tổng thể 
+$(document).ready(function () {
+    var owl = $("#slideshow");
+    owl.owlCarousel({
+        center: true,
+        loop: false,
+        dots: false,
+        nav: false,
+        items: 1,
+    });
+
+    // listen change state menu tabs when slide was changed 
+    owl.on('changed.owl.carousel', function (event) {
+        var tabs = document.querySelectorAll('.tab_navigation li');
+        var tabActive = document.querySelector('.tab_navigation li.active')
+        tabActive?.classList.remove("active")
+        tabs[event.item.index]?.classList.add("active")
+    })
+
+
+    // Change content slide when clicked to menu tabs
+    $('.tab_navigation li').click(function (event) {
+        index = $(this).index();
+        $('.tab_navigation li').removeClass('active');
+        $(this).addClass('active');
+        $("#slideshow").trigger("to.owl.carousel", [index, 1])
+    });
+
+});
+
+
+// Giải đáp những thắc mắc về  Phần mềm
+$(document).ready(function () {
+
+    // set first tab is actived
+    $('#tabs .tab_content:first').show();
+    $('#tabs .item:first').addClass('active');
+    
+
+    //Change content when click to tabs
+    $('#tabs .item').click(function (event) {
+        index = $(this).index();
+        if ($(this).hasClass("active")) {
+            $('#tabs .tab_content').hide();
+            $('#tabs .item').removeClass('active');
+        } else {
+            $('#tabs .item').removeClass('active');
+            $(this).addClass('active');
+            $('#tabs .tab_content').hide();
+            $('#tabs .tab_content').eq(index).show();
+        }
+    });
+});
+
+// Back to top
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop()) {
+            $('#backTop').fadeIn();
+        } else {
+            $('#backTop').fadeOut();
+        }
+    });
+});
+
+$('#backTop').click(function (e) {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 0);
+})
